bca2aac655c06db1aa8c10e7cb0818ceaeebdc42
Ramya H S <ramyashivu910@gmail.com>
2021-06-19 00:29:24 +0530

    Changed project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('assignment-one app is running!');
+    expect(await page.getTitleText()).toEqual('finding-falcone app is running!');
   });
 
   afterEach(async () => {
